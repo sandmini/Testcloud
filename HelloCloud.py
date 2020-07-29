e3137e0cd378e164e0949b8026a69d6c1362705d
sandmini <39480731+sandmini@users.noreply.github.com>
2020-07-29 17:58:10 +0700

    Third Commit

--- HelloCloud.py
+++ HelloCloud.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1>Hello There!</h1>'
+    return '<h1>Hello There EWEW</h1>'
 @app.route('/home',methods=['GET','POST'])
 def home():
     links = ['https://www.youtube.com','https://www.bing.com',
